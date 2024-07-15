8d1feaf52c9364c07737d1317bbdebe90299c344
alex.mosisa <alex.mosisa@chisw.com>
2024-07-15 12:26:05 +0300

    set minimum windiw size

--- fiofan_test_task/lib/main.dart
+++ fiofan_test_task/lib/main.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:window_size/window_size.dart' as window_size;
+import 'dart:io' show Platform;
 
 import 'presentation/screens/score_board_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+    // Set the initial window size
+    window_size.setWindowFrame(const Rect.fromLTWH(0, 0, 1100.0, 600.0));
+
+    // Set the minimum window size
+    window_size.setWindowMinSize(const Size(1100.0, 600.0));
+  }
   runApp(const MyApp());
 }
 
@@ -13,7 +23,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-        designSize: const Size(360, 690),
+        designSize: const Size(500, 690),
         minTextAdapt: true,
         splitScreenMode: true,
         builder: (_, child) {
